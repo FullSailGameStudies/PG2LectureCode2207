997421244e660f978b20800b14b03ed29688e733
Garrett Girod <girodg@hotmail.com>
2022-07-06 12:08:56 -0400

    Fibonacci

--- Day05/Day05/Program.cs
+++ Day05/Day05/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Day02
@@ -29,7 +30,47 @@ namespace Day02
             int N = 5;
             long result = Factorial(N);
             Console.WriteLine($"{N}! = {result}");
+
+            //0,0
+            //1,1
+            _fib.Add(0, 0);
+            _fib[1] = 1;
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < 145; i++)
+            {
+                sw.Restart();
+                ulong fib = Fibonacci2(i);
+                sw.Stop();
+                long ms = sw.ElapsedMilliseconds;
+                Console.Write($"Fib({i}) = {fib}");
+                Console.CursorLeft = 50;
+                Console.WriteLine($"{ms} ms");
+            }
         }
+
+        static Dictionary<int, ulong> _fib = new Dictionary<int, ulong>();
+
+        static ulong Fibonacci2(int N)
+        {
+            //exit condition
+            if (_fib.TryGetValue(N, out ulong result))
+                return result;
+
+            //Fib(N) = Fib(N-1) + Fib(N-2)
+            result = Fibonacci2(N - 1) + Fibonacci2(N - 2);
+            _fib.Add(N, result);
+            return result;
+        }//O(N) - linear
+
+        static ulong Fibonacci(int N)
+        {
+            if (N == 0) return 0;
+            if (N == 1) return 1;
+
+            //Fib(N) = Fib(N-1) + Fib(N-2)
+            ulong result = Fibonacci(N - 1) + Fibonacci(N - 2);
+            return result;
+        }//O(2^N)
 
         static long Factorial(int N)
         {
