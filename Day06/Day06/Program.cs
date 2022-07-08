0871418ed2e9ee3bed42e908d5cfdf3800a400fc
Garrett Girod <girodg@hotmail.com>
2022-07-08 10:44:08 -0400

    Split

--- Day06/Day06/Program.cs
+++ Day06/Day06/Program.cs
@@ -15,6 +15,9 @@ namespace Day06
                 Console.Write($"{numbers[i]} ");
             }
 
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            Split(nums);
+
             string s1 = "Batman", s2 = "Aquaman";
             //string1.CompareTo(string2)
             //returns...
@@ -25,6 +28,31 @@ namespace Day06
             if (compResult == 0) Console.WriteLine($"{s1} EQUALS {s2}");
             else if(compResult == -1) Console.WriteLine($"{s1} LESS THAN {s2}");
             else if (compResult == 1) Console.WriteLine($"{s1} GREATER THAN {s2}");
+        }
+
+        static void Split(List<int> numbers)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new();
+            int mid = numbers.Count / 2;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i < mid) left.Add(numbers[i]);
+                else right.Add(numbers[i]);
+            }
+
+            Console.WriteLine("\n\n-------LEFT-------");
+            foreach (var item in left)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine("\n\n-------RIGHT-------");
+            foreach (var item in right)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
         }
 
         static void Swap(double[] nums, int index1, int index2)
