afcb2893e165f963162f3bac5e1931f4e97df3da
Garrett Girod <girodg@hotmail.com>
2022-07-15 10:17:48 -0400

    extension methods

---
+++ Day07/Day07CL/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public static class Extensions
+    {
+        public static int CastSpell(this FantasyWeapon weapon)
+        {
+            Random random = new Random();
+            int damage = random.Next(20) + weapon.DoDamage();
+            return damage;
+        }
+    }
+}
