dcba2a17bc3cd4c82cf0e892a4dbcf4f4835f2b3
Garrett Girod <girodg@hotmail.com>
2022-07-16 14:58:22 -0400

    Week 03 Group Chat

---
+++ GroupChatWeek3/GroupChat03/GroupChat03/PG2Sorting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupChat03
+{
+    public static class PG2Sorting
+    {
+        public static void BubbleSort(List<string> A)
+        {
+            int n = A.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i <= n-1; i++)
+                {
+                    if (A[i - 1].CompareTo(A[i]) > 0)
+                    {
+                        string temp = A[i - 1];
+                        A[i - 1] = A[i];
+                        A[i] = temp;
+                        swapped = true;
+                    }
+                }
+                --n;
+            } while (swapped);
+        }
+    }
+}
---
+++ GroupChatWeek3/GroupChat03/GroupChat03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupChat03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileText = File.ReadAllText("inputFile.csv");
+            List<string> comics = fileText.Split(',').ToList();
+            List<string> sorted = comics.ToList();
+            PG2Sorting.BubbleSort(sorted);
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
